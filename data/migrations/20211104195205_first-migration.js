724d67667a5bd6194fd7a6043d22875bc55979f3
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-04 13:07:50 -0700

    recipes db created

--- data/migrations/20211104195205_first-migration.js
+++ data/migrations/20211104195205_first-migration.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('recipes', (table) => {
+      table.increments('recipe_id');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('recipes');
 };
