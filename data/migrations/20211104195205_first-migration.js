66835eb6f87a1f2bd701248c311c4a614b352bc3
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-04 15:51:51 -0700

    sql database fleshed out. hopefully its designed well.

--- data/migrations/20211104195205_first-migration.js
+++ data/migrations/20211104195205_first-migration.js
@@ -1,10 +1,44 @@
-
-exports.up = function(knex) {
-  return knex.schema.createTable('recipes', (table) => {
-      table.increments('recipe_id');
-  });
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipes", (table) => {
+      table.increments("recipe_id");
+      table.string("recipe_name", 64).notNullable();
+      table.string("created_at");
+    })
+    .createTable("steps", (table) => {
+      table.increments("steps");
+      table.integer("step_number").notNullable();
+      table.string("step_name", 64).notNullable();
+      table.string("instruction", 128).notNullable();
+    })
+    .createTable("ingredients", (table) => {
+      table.increments("ingredients_id");
+      table.string("ingredient_name", 128).notNullable();
+      table.integer("quantity").notNullable();
+    })
+    .createTable("steps_ingredients", (table) => {
+      table.increments("steps_ingredients_id");
+      table
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id").inTable("steps")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      table
+        .integer("ingredients_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredients_id").inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('recipes');
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("steps_ingredients")
 };
